cebfe4d7b6fa65211a0b334b623154c1ea5782b0
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-06-14 16:29:02 +0600

    axios secure is added

--- src/pages/Classes/ClassCard.jsx
+++ src/pages/Classes/ClassCard.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const ClassCard = ({allClass}) => {
     const { user } = useAuth()
-    console.log(user)
+    const isAvailable = allClass?.availableSeats > 0;
+
+    const handleSelect = () => {
+        if (!user){
+            Swal.fire({
+                position: 'top-end',
+                icon: 'warning',
+                title: 'You need to login first',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+    }
+
   return (
-    <div className="class-card bg-white p-4 rounded shadow grid-item">
+    <div className={`class-card p-4 rounded shadow grid-item ${
+        !isAvailable ? "bg-red-500" : "bg-white"
+      }`}>
       <img
         src={allClass.image}
         alt={allClass.name}
@@ -17,7 +33,9 @@ const ClassCard = ({allClass}) => {
           Instructor: {allClass.instructor}
         </p>
         <p>Total Students: {allClass.totalStudents}</p>
+        <p>Available Seats: {allClass.availableSeats}</p>
         <p>Price: ${allClass.price}</p>
+        <button onClick={handleSelect} className="btn btn-info mt-2">Select</button>
       </div>
     </div>
   );
